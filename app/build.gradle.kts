a8583aa4468c0599a6b6bc07acfda8677da74740
Nuno Rebelo <a2022137005@isec.pt>
2026-03-17 16:53:10 +0000

    Unit Tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.3")
 
     testImplementation(libs.junit)
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(kotlin("test"))
 }
